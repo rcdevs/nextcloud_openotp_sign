e4b7dc904d6f5616a0c0ce930020ae9f19a8879e
Florent CHANTRET <florent.chantret@rcdevs.com>
2021-10-08 17:48:56 +0200

    Fixes to pass ESLint checks

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-"use strict";
+'use strict'
 
 OCA.Files.fileActions.registerAction({
-    mime: 'file',
-    name: 'OpenOTPSign',
-    permissions: OC.PERMISSION_READ,
-    iconClass: 'custom-icon-signature',
-    actionHandler: function(filename, context) {
-        alert("TODO");
-    },
-    displayName: t('openotpsign', 'Sign with OpenOTP'),
-    order: -100
-});
+	mime: 'file',
+	name: 'OpenOTPSign',
+	permissions: OC.PERMISSION_READ,
+	iconClass: 'custom-icon-signature',
+	actionHandler: (filename, context) => {
+		alert('TODO')
+	},
+	displayName: t('openotpsign', 'Sign with OpenOTP'),
+	order: -100,
+})
