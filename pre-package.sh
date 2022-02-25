9292a13180ee26fbe9a6b7b66d9d037772d9bb44
Florent CHANTRET <florent.chantret@rcdevs.com>
2022-02-25 16:02:49 +0100

    Add pre-package.sh script

---
+++ pre-package.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+make
+rm -f babel.config.js
+rm -rf build/
+rm -f composer.json
+rm -f composer.lock
+rm -f .eslintrc.js
+rm -rf .git/
+rm -f .gitignore
+rm -f Makefile
+rm -rf node_modules/
+rm -f package.json
+rm -f package-lock.json
+rm -f phpunit.integration.xml
+rm -f phpunit.xml
+rm -f README.md
+rm -rf src/
+rm -f stylelint.config.js
+rm -rf tests/
+rm -rf translationfiles/
+rm -f .travis.yml
+rm -f webpack.js
+rm -f pre-package.sh
