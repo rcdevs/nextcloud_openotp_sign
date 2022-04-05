15bcdba3d732f5fddc8075be905c9027d63cdc7f
Florent CHANTRET <florent.chantret@rcdevs.com>
2022-04-05 12:59:36 +0200

    Factorize css styles

--- src/App.vue
+++ src/App.vue
@@ -39,4 +39,78 @@
 .app-container {
 	width: 100%;
 }
+
+#app-content >>> h1 {
+	font-size: 2em;
+	text-align: center;
+}
+
+#app-content >>> table {
+	border-collapse: collapse;
+	margin: 25px 0;
+	font-size: 0.9em;
+	font-family: sans-serif;
+	width: 100%;
+	box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+}
+
+#app-content >>> thead tr {
+	background-color: var(--color-primary-element-light);
+	color: #ffffff;
+	text-align: left;
+}
+
+#app-content >>> th, #app-content >>> td {
+	padding: 12px 15px;
+}
+
+#app-content >>> tr {
+	border-bottom: 1px solid #dddddd;
+}
+
+#app-content >>> tr:last-of-type {
+	border-bottom: 2px solid var(--color-primary-element-light);
+}
+
+#app-content >>> img {
+	display: block;
+	margin: 25px auto;
+}
+
+#app-content >>> .component-container {
+	margin: 20px;
+	width: 100%;
+}
+
+#app-content >>> .pagination {
+	list-style: none;
+	padding: 0;
+}
+
+#app-content >>> .pagination li {
+	float: left;
+}
+
+#app-content >>> .pagination a {
+	display: block;
+	border: solid #dddddd 1px;
+	padding: 6px 12px;
+	margin-right: -1px;
+	color: var(--color-primary-element-light);
+}
+
+#app-content >>> .pagination .active a {
+	color: var(--color-main-background);
+	background-color: var(--color-primary-element-light);
+	cursor: default;
+}
+
+#app-content >>> .pagination .disabled a {
+	color: #dddddd;
+	cursor: not-allowed;
+}
+
+#app-content >>> .pagination li:not(.active):not(.disabled) a:hover {
+	background-color: #dddddd;
+}
 </style>
